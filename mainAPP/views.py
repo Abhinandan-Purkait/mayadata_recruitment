e93fc574133a1b53710bdff874309788295c0c22
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2020-04-30 13:08:55 +0530

    new graph

--- mainAPP/views.py
+++ mainAPP/views.py
@@ -45,11 +45,34 @@ fig2 = px.scatter_geo(formated_gdf, locations="Country/Region", locationmode='co
 plot_div2 = plot(fig2, output_type='div', include_plotlyjs=False, show_link=False,
                  link_text="", image_width=500, config={"displaylogo": False})
 
+data['Province/State'] = data['Province/State'].fillna('')
+temp = data[[col for col in data.columns if col != 'Province/State']]
+
+latest = temp[temp['date'] == max(temp['date'])].reset_index()
+latest_grouped = latest.groupby(
+    'Country/Region')['Confirmed', 'Deaths'].sum().reset_index()
+fig3 = px.bar(latest_grouped.sort_values('Confirmed', ascending=False)[:30][::-1],
+              x='Confirmed', y='Country/Region',
+              title='Confirmed Cases Worldwide', text='Confirmed', height=1000, orientation='h')
+plot_div3 = plot(fig3, output_type='div', include_plotlyjs=False, show_link=False,
+                 link_text="", image_width=500, config={"displaylogo": False})
+
 
 def index(request):
     global plot_div1, plot_div2
-    return render(request, "index.html", context={'plot_div1': plot_div1, 'plot_div2': plot_div2})
+    return render(request, "index.html", context={'plot_div1': plot_div1, 'plot_div2': plot_div2, 'plot_div3': plot_div3})
 
 
 def about(request):
     return render(request, "abhi.html")
+
+
+data['Province/State'] = data['Province/State'].fillna('')
+temp = data[[col for col in data.columns if col != 'Province/State']]
+
+latest = temp[temp['date'] == max(temp['date'])].reset_index()
+latest_grouped = latest.groupby(
+    'Country/Region')['Confirmed', 'Deaths'].sum().reset_index()
+fig3 = px.bar(latest_grouped.sort_values('Confirmed', ascending=False)[:30][::-1],
+              x='Confirmed', y='Country/Region',
+              title='Confirmed Cases Worldwide', text='Confirmed', height=1000, orientation='h')
