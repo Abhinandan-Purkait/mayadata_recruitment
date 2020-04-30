5c5a87f1d6beee0d016d3af82252c6184a253cf0
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2020-04-30 12:19:26 +0530

    app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mayadata_rec.wsgi
---
+++ mainAPP/views.py
@@ -0,0 +1,55 @@
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+from django.shortcuts import render
+
+import numpy as np
+import pandas as pd
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = "plotly_dark"
+
+data = pd.read_csv("train.csv", parse_dates=['Date'])
+cleaned_data = pd.read_csv(
+    "covid_19_clean_complete.csv", parse_dates=['Date'])
+cases = ['Confirmed', 'Deaths', 'Recovered', 'Active']
+cleaned_data['Active'] = cleaned_data['Confirmed'] - \
+    cleaned_data['Deaths'] - cleaned_data['Recovered']
+cleaned_data['Country/Region'] = cleaned_data['Country/Region'].replace(
+    'Mainland China', 'China')
+cleaned_data[['Province/State']
+             ] = cleaned_data[['Province/State']].fillna('')
+cleaned_data[cases] = cleaned_data[cases].fillna(0)
+cleaned_data.rename(columns={'Date': 'date'}, inplace=True)
+data = cleaned_data
+formated_gdf = data.groupby(
+    ['date', 'Country/Region'])['Confirmed', 'Deaths'].max()
+formated_gdf = formated_gdf.reset_index()
+formated_gdf['date'] = pd.to_datetime(formated_gdf['date'])
+formated_gdf['date'] = formated_gdf['date'].dt.strftime('%m/%d/%Y')
+formated_gdf['size'] = formated_gdf['Confirmed'].pow(0.3)
+
+fig1 = px.scatter_geo(formated_gdf, locations="Country/Region", locationmode='country names',
+                      color="Confirmed", size='size', hover_name="Country/Region",
+                      range_color=[0, 1500],
+                      projection="natural earth", animation_frame="date",
+                      title='COVID-19: Spread Over Time', color_continuous_scale="portland")
+plot_div1 = plot(fig1, output_type='div', include_plotlyjs=False, show_link=False,
+                 link_text="", image_width=500, config={"displaylogo": False})
+fig2 = px.scatter_geo(formated_gdf, locations="Country/Region", locationmode='country names',
+                      color="Deaths", size='size', hover_name="Country/Region",
+                      range_color=[0, 100],
+                      projection="natural earth", animation_frame="date",
+                      title='COVID-19: Deaths Over Time', color_continuous_scale="peach")
+plot_div2 = plot(fig2, output_type='div', include_plotlyjs=False, show_link=False,
+                 link_text="", image_width=500, config={"displaylogo": False})
+
+
+def index(request):
+    global plot_div1, plot_div2
+    return render(request, "index.html", context={'plot_div1': plot_div1, 'plot_div2': plot_div2})
+
+
+def about(request):
+    return render(request, "abhi.html")
